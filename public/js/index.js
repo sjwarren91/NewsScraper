0e05d0205a04394b6f99adbb6791d2e9c29fbebd
Sam Warren <sjwarren91@gmail.com>
2019-10-20 10:30:57 +1100

    scrape styling and notification

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-  $.get("/articles", data => {
-    $(".container").html(data);
-  }).then(function() {
-    scrapeUpdate();    
-  });
+  scrapeUpdate()
 });
 
 function scrapeUpdate() {
   $.get("/scrape", function() {
   }).done(function() {
+    $(".modal2").css("display", "none");
     $.get("/articles", function(data) {
       $(".container").html(data);    
     });
   });
 }
+
+$(".scrape").on("click", function() {
+  $(".modal2").css("display", "block");
+  scrapeUpdate();
+})
